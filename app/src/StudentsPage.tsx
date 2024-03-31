5021523d63f111ee012c7376fc0ddf46fe1ee72c
Lilach E <lilachelayza@gmail.com>
2024-03-31 12:18:38 +0300

    display student list on students page

--- app/src/StudentsPage.tsx
+++ app/src/StudentsPage.tsx
@@ -1,5 +1,6 @@
-import { useLoaderData } from "react-router";
 import axios from "axios";
+import { useEffect, useState } from "react";
+import styles from "./StudentsPage.module.scss";
 
 type Student = {
   id: string;
@@ -9,13 +10,36 @@ type Student = {
 };
 
 export async function loader() {
-  const res = await axios.get<Student[]>("http://localhost:3000/students");
-
-  return res.data;
+  try {
+    const res = await axios.get<Student[]>("http://localhost:3000/students");
+    return res.data;
+  } catch (err) {
+    console.error("Error fetching students:", err);
+    return [];
+  }
 }
 
 export default function StudentsPage() {
-  const data = useLoaderData();
+  const [students, setStudents] = useState<Student[]>([]);
 
-  return <pre>{JSON.stringify(data, null, 4)}</pre>;
+  useEffect(() => {
+    async function fetchData() {
+      const data = await loader();
+      setStudents(data);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <ul className={styles.studentList}>
+      {students.map((student) => (
+        <li className={styles.studentList__Item} key={student.id}>
+          <span>
+            Name: {student.firstName} {student.lastName}
+          </span>
+          <span>email: {student.email}</span>
+        </li>
+      ))}
+    </ul>
+  );
 }
